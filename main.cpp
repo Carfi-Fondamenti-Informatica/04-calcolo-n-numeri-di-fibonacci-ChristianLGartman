d74ab49fff11cafb7773c177cb7e6f0171655899
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-22 11:02:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
-
+int numeridiFibonacci(int n){
+    int a=0, b=1, c=0;
+    if(n>=2){
+        cout<<b<<endl;
+        for (int i=1; i<n; i++) {
+         c=a+b;
+         a=b;
+         b=c;
+         cout<<c<<endl;
+        }
+    }
+    else{
+        cout<<"errore";
+    }
+    return n;
+}
 int main() {
-   return 0;
+    int p;
+    cout << "dimmi quanti numeri della Serie di Fibonacci vuoi che stampi:"<< endl;
+    cin >> p;
+    int h = numeridiFibonacci(p);
+    return 0;
 }
